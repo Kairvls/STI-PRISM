151307f0afcd9c38d5b84dc30b02e3df52169de1
Jhon Carlo Suan <jhoncarlosuan26@gmail.com>
2026-07-21 02:04:46 +0800

    building layout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -390,13 +390,9 @@ class AdminController extends Controller
                 'reports_table.report_unlisted_equipment_name',
                 'equipment_table.equipment_name'
             )
-<<<<<<< HEAD
-            ->whereNotNull('requisition_issue_slip_table.ris_requested_by_date')
-=======
             ->where('requisition_issue_slip_table.ris_status', 'Pending')
             ->whereNotNull('requisition_issue_slip_table.ris_requested_by_date')
             ->whereNull('requisition_issue_slip_table.ris_approved_by_date')
->>>>>>> d8daffc056cc5a9fb69d71a51889a26aaa65dab1
             ->orderByDesc('requisition_issue_slip_table.ris_requested_by_date')
             ->paginate(10);
 
@@ -424,11 +420,7 @@ class AdminController extends Controller
             DB::table('requisition_issue_slip_table')
                 ->where('ris_id', $risId)
                 ->update([
-<<<<<<< HEAD
-                    'ris_status' => 'Approved',
-=======
                     'ris_status' => 'Pending',
->>>>>>> d8daffc056cc5a9fb69d71a51889a26aaa65dab1
                     'ris_approved_by_signature' => Auth::user()->user_full_name ?? 'Admin',
                     'ris_approved_by_date' => now()->toDateString(),
                 ]);
